cf18b2de475d6594434e8012f3e229d0d87a5fbb
Austen Perelman-Hall <austenph@gmail.com>
2017-01-13 14:51:49 -0500

    This is my first commit

---
+++ FortuneTellerMethod-ProjectTwo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortuneTellerMethod_ProjectTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
